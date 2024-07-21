342bd944e9bec64d2ec79e649ee6da8bbbd5bcd5
EdMifort <edgar.papikyan@mifort.org>
2024-07-21 21:00:17 +0400

    fixed kotlin jks duplicate issues

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ void main() async {
 
 class App extends StatelessWidget {
   const App({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(

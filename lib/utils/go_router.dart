8cdb8ec5be3b6e0b52e213177190bb491173c26f
EdMifort <edgar.papikyan@mifort.org>
2024-07-29 18:25:24 +0400

    additional commit

--- lib/utils/go_router.dart
+++ lib/utils/go_router.dart
@@ -3,12 +3,18 @@ import 'package:denari_app/view/screens/send_gift_screen/send_gift_card_screen.d
 import 'package:denari_app/view/widgets/brand_item/brand_item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import '../view/screens/authentication/code/code_screen.dart';
+import '../view/screens/authentication/forgot/forgot_screen.dart';
+import '../view/screens/authentication/password/create_password_screen.dart';
+import '../view/screens/authentication/sign_in/sign_in_screen.dart';
+import '../view/screens/authentication/sign_up/sign_up_screen.dart';
 import '../view/screens/send_gift_screen/send_gift_screen.dart';
 import '../view/screens/shop_screen.dart';
 import '../view/screens/main_screen/main_screen.dart';
 import '../view/screens/notification_screen.dart';
 import '../view/screens/profile_screen.dart';
 import '../view/widgets/scaffold_nav_bar.dart';
+import 'listeners/auth_listener.dart';
 
 final GoRouter router = GoRouter(
   routes: [
@@ -66,66 +72,66 @@ final GoRouter router = GoRouter(
               }
           ),
         ]),
-    // GoRoute(
-    //   name: Routes.signIn,
-    //   path: '/${Routes.signIn}',
-    //   builder: (context, state) => const SignInScreen(),
-    //   routes: [
-    //     GoRoute(
-    //       name: Routes.forgot,
-    //       path: Routes.forgot,
-    //       builder: (context, state) => const ForgotScreen(),
-    //       routes: [
-    //         GoRoute(
-    //           name: Routes.forgotCode,
-    //           path: Routes.code,
-    //           builder: (context, state) =>
-    //               CodeScreen(phone: state.extra as String),
-    //           routes: [
-    //             GoRoute(
-    //               name: Routes.password,
-    //               path: Routes.password,
-    //               builder: (context, state) =>
-    //                   CreatePasswordScreen(phone: state.extra as String),
-    //             ),
-    //           ],
-    //         ),
-    //       ],
-    //     ),
-    //   ],
-    // ),
-    // GoRoute(
-    //   name: Routes.signUp,
-    //   path: '/${Routes.signUp}',
-    //   builder: (context, state) => const SignUpScreen(),
-    //   routes: [
-    //     GoRoute(
-    //       name: Routes.code,
-    //       path: Routes.code,
-    //       builder: (context, state) => CodeScreen(phone: state.extra as String),
-    //     ),
-    //   ],
-    // ),
+    GoRoute(
+      name: Routes.signIn,
+      path: '/${Routes.signIn}',
+      builder: (context, state) => const SignInScreen(),
+      routes: [
+        GoRoute(
+          name: Routes.forgot,
+          path: Routes.forgot,
+          builder: (context, state) => const ForgotScreen(),
+          routes: [
+            GoRoute(
+              name: Routes.forgotCode,
+              path: Routes.code,
+              builder: (context, state) =>
+                  CodeScreen(phone: state.extra as String),
+              routes: [
+                GoRoute(
+                  name: Routes.password,
+                  path: Routes.password,
+                  builder: (context, state) =>
+                      CreatePasswordScreen(phone: state.extra as String),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+    GoRoute(
+      name: Routes.signUp,
+      path: '/${Routes.signUp}',
+      builder: (context, state) => const SignUpScreen(),
+      routes: [
+        GoRoute(
+          name: Routes.code,
+          path: Routes.code,
+          builder: (context, state) => CodeScreen(phone: state.extra as String),
+        ),
+      ],
+    ),
   ],
-//   refreshListenable: authListener,
-//   redirect: (context, state) {
-//     final path = state.uri.path;
-//     final login = authListener.isLogin;
-//     if (login && (path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
-//       return '/';
-//     }
-//     if (!login && !(path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
-//       return '/${Routes.signIn}';
-//     }
-//     return null;
-//   },
+  refreshListenable: authListener,
+  redirect: (context, state) {
+    final path = state.uri.path;
+    final login = authListener.isLogin;
+    if (login && (path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
+      return '/';
+    }
+    if (!login && !(path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
+      return '/${Routes.signIn}';
+    }
+    return null;
+  },
 );
-//
-// final class Routes {
-//   static const signIn = 'sign-in';
-//   static const signUp = 'sign-up';
-//   static const forgot = 'forgot';
-//   static const code = 'code';
-//   static const forgotCode = 'f-code';
-//   static const password = 'password';
-// }
+
+final class Routes {
+  static const signIn = 'sign-in';
+  static const signUp = 'sign-up';
+  static const forgot = 'forgot';
+  static const code = 'code';
+  static const forgotCode = 'f-code';
+  static const password = 'password';
+}

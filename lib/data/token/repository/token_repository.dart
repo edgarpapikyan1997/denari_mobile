6f3cfb8b1dce866e50c4cb3dbfb9ba0c78eac193
EdMifort <edgar.papikyan@mifort.org>
2024-08-01 16:57:26 +0400

    deleted unnneded imports in token repo

--- lib/data/token/repository/token_repository.dart
+++ lib/data/token/repository/token_repository.dart
@@ -1,9 +1,5 @@
-import 'package:denari_app/data/authentication/model/login_model.dart';
-import 'package:denari_app/data/authentication/model/reg_model.dart';
-import 'package:denari_app/data/authentication/model/reset_model.dart';
 import 'package:denari_app/data/token/model/token_balance/token_balance_model.dart';
 import 'package:denari_app/data/token/model/token_model.dart';
-import 'package:denari_app/utils/network/model/api_token.dart';
 
 abstract class TokenRepository {
   Future<List<TokenModel>> getUserTokenBalanceHistory();

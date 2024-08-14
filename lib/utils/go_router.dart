71e8d8bea58fef535e0e80f6e90f84f939773144
EdMifort <edgar.papikyan@mifort.org>
2024-08-14 14:56:55 +0400

    addition commit to shop_screen

--- lib/utils/go_router.dart
+++ lib/utils/go_router.dart
@@ -16,7 +16,14 @@ import 'package:denari_app/view/screens/store_field_screen/store_field_item_scre
 import 'package:denari_app/view/widgets/brand_item/brand_item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import '../data/authentication/model/reg_model.dart';
 import '../data/authentication/model/reset_pass_model.dart';
+import '../view/screens/authentication/code/forgot_code_screen.dart';
+import '../view/screens/authentication/code/sign_up_code_screen.dart';
+import '../view/screens/authentication/forgot/forgot_screen.dart';
+import '../view/screens/authentication/password/create_password_screen.dart';
+import '../view/screens/authentication/sign_in/sign_in_screen.dart';
+import '../view/screens/authentication/sign_up/sign_up_screen.dart';
 import '../view/screens/main_screen/main_screen.dart';
 import '../view/screens/main_screen/my_gift_cards_screen.dart';
 import '../view/screens/map_screen/map_screen.dart';
@@ -25,6 +32,7 @@ import '../view/screens/profile/profile_screen.dart';
 import '../view/screens/send_gift_screen/send_gift_screen.dart';
 import '../view/screens/shops_screen/shop_screen.dart';
 import '../view/widgets/scaffold_nav_bar.dart';
+import 'listeners/auth_listener.dart';
 
 final GoRouter router = GoRouter(
   routes: [
@@ -95,7 +103,9 @@ final GoRouter router = GoRouter(
         GoRoute(
             path: '/alliance',
             builder: (context, state) {
-              return AllianceScreen(isAlliance: state.extra as bool?,);
+              return AllianceScreen(
+                isAlliance: state.extra as bool?,
+              );
             }),
         GoRoute(
           name: Routes.profile,
@@ -152,10 +162,11 @@ final GoRouter router = GoRouter(
       ],
     ),
     GoRoute(
-        path: '/mapScreen',
-        builder: (context, state) {
-          return const MapScreen();
-        },),
+      path: '/mapScreen',
+      builder: (context, state) {
+        return const MapScreen();
+      },
+    ),
     GoRoute(
         path: '/sendGift',
         builder: (context, state) {
@@ -171,62 +182,62 @@ final GoRouter router = GoRouter(
                 return SendGiftCardScreen(brandItemWidget: brandItemWidget);
               }),
         ]),
-    // GoRoute(
-    //   name: Routes.signIn,
-    //   path: '/${Routes.signIn}',
-    //   builder: (context, state) => const SignInScreen(),
-    //   routes: [
-    //     GoRoute(
-    //       name: Routes.forgot,
-    //       path: Routes.forgot,
-    //       builder: (context, state) => const ForgotScreen(),
-    //       routes: [
-    //         GoRoute(
-    //           name: Routes.forgotCode,
-    //           path: Routes.code,
-    //           builder: (context, state) =>
-    //               ForgotCodeScreen(model: state.extra as ResetPassModel),
-    //           routes: [
-    //             GoRoute(
-    //               name: Routes.password,
-    //               path: Routes.password,
-    //               builder: (context, state) => CreatePasswordScreen(
-    //                   model: state.extra as ResetPassModel),
-    //             ),
-    //           ],
-    //         ),
-    //       ],
-    //     ),
-    //   ],
-    // ),
-    // GoRoute(
-    //   name: Routes.signUp,
-    //   path: '/${Routes.signUp}',
-    //   builder: (context, state) => const SignUpScreen(),
-    //   routes: [
-    //     GoRoute(
-    //       name: Routes.code,
-    //       path: Routes.code,
-    //       builder: (context, state) =>
-    //           SignUpCodeScreen(model: state.extra as RegModel),
-    //     ),
-    //   ],
-    // ),
+    GoRoute(
+      name: Routes.signIn,
+      path: '/${Routes.signIn}',
+      builder: (context, state) => const SignInScreen(),
+      routes: [
+        GoRoute(
+          name: Routes.forgot,
+          path: Routes.forgot,
+          builder: (context, state) => const ForgotScreen(),
+          routes: [
+            GoRoute(
+              name: Routes.forgotCode,
+              path: Routes.code,
+              builder: (context, state) =>
+                  ForgotCodeScreen(model: state.extra as ResetPassModel),
+              routes: [
+                GoRoute(
+                  name: Routes.password,
+                  path: Routes.password,
+                  builder: (context, state) => CreatePasswordScreen(
+                      model: state.extra as ResetPassModel),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+    GoRoute(
+      name: Routes.signUp,
+      path: '/${Routes.signUp}',
+      builder: (context, state) => const SignUpScreen(),
+      routes: [
+        GoRoute(
+          name: Routes.code,
+          path: Routes.code,
+          builder: (context, state) =>
+              SignUpCodeScreen(model: state.extra as RegModel),
+        ),
+      ],
+    ),
   ],
-  // refreshListenable: authListener,
-  // redirect: (context, state) {
-  //   final path = state.uri.path;
-  //   final login = authListener.isLogin;
-  //   if (login &&
-  //       (path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
-  //     return '/';
-  //   }
-  //   if (!login &&
-  //       !(path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
-  //     return '/${Routes.signIn}';
-  //   }
-  //   return null;
-  // },
+  refreshListenable: authListener,
+  redirect: (context, state) {
+    final path = state.uri.path;
+    final login = authListener.isLogin;
+    if (login &&
+        (path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
+      return '/';
+    }
+    if (!login &&
+        !(path.contains(Routes.signUp) || path.contains(Routes.signIn))) {
+      return '/${Routes.signIn}';
+    }
+    return null;
+  },
 );
 
 final class Routes {
